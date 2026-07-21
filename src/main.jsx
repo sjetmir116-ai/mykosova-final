3a22548fe50181d3882bd13281b2417314efd7da
sjetmir116-ai <sjetmir116@gmail.com>
2026-07-21 22:11:44 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // ose App.css nëse nuk ke index.css
+import "./index.css";
+
 
 const container = document.getElementById('root')
 const root = createRoot(container)
