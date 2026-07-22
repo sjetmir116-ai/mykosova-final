7edb20c08f8690fcd3e45de9d28ae3191984e508
sjetmir116-ai <sjetmir116@gmail.com>
2026-07-23 00:42:14 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -18,3 +18,26 @@ const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 
 const analytics = getAnalytics(app);
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCo720b5hAPjp-taEH78nMs5G7DSE8FKGk",
+  authDomain: "my-kosova.firebaseapp.com",
+  databaseURL: "https://my-kosova-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "my-kosova",
+  storageBucket: "my-kosova.firebasestorage.app",
+  messagingSenderId: "766501162220",
+  appId: "1:766501162220:web:211fb4f10fa55fdf2d29c9",
+  measurementId: "G-SL8JVCJGK8"
+};
+
+// Inicializimi i aplikacionit
+const app = initializeApp(firebaseConfig);
+
+// Eksportimi i Firestore për t'u përdorur në skedarët e tjerë
+export const db = getFirestore(app);
+
+// Inicializimi i Analytics
+const analytics = getAnalytics(app);
