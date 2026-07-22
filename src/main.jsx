b9e68c5b7fdcc486cee3a3bb557e143e1c62d821
sjetmir116-ai <sjetmir116@gmail.com>
2026-07-22 22:05:08 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ListaBizneseve from './ListaBizneseve.jsx'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ListaBizneseve />
+    <App />
   </React.StrictMode>,
 )
