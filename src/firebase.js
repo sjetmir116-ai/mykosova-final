f7566671751645a5dfcf6383f0c4936710f3375c
sjetmir116-ai <sjetmir116@gmail.com>
2026-07-23 00:41:07 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,20 @@
 import { initializeApp } from "firebase/app";
-import { initializeFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA8",
-  authDomain: "://firebaseapp.com",
-  projectId: "mykosova-final",
-  storageBucket: "://appspot.com",
-  messagingSenderId: "105", 
-  appId: "1:105"
+  apiKey: "AIzaSyCo720b5hAPjp-taEH78nMs5G7DSE8FKGk",
+  authDomain: "my-kosova.firebaseapp.com",
+  databaseURL: "https://my-kosova-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "my-kosova",
+  storageBucket: "my-kosova.firebasestorage.app",
+  messagingSenderId: "766501162220",
+  appId: "1:766501162220:web:211fb4f10fa55fdf2d29c9",
+  measurementId: "G-SL8JVCJGK8"
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const db = initializeFirestore(app, {
-  experimentalForceLongPolling: true,
-});
+export const db = getFirestore(app);
+
+const analytics = getAnalytics(app);
