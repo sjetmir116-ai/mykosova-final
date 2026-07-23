79bdf3f436cc5a23cad451fdf14e99810f67ed4e
sjetmir116-ai <sjetmir116@gmail.com>
2026-07-23 11:26:14 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { AppProvider } from './AppContext.jsx' // <--- Kjo rresht mungonte!
-import './index.css'
+import './App.css'
 
 const container = document.getElementById('root')
 const root = createRoot(container)
